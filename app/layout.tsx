6bb0d73fdae155ade060794f947b4c2635cb7ee8
George Siosi Samuels <george@faiacorp.com>
2025-03-10 00:04:20 -0600

    feat(auth): explicitly set publishableKey in ClerkProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default async function RootLayout({
           appearance={{
             baseTheme: undefined
           }}
+          publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
         >
           <Providers>
             {children}
