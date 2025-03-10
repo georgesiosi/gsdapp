9f0b958cf58a7e1404f2a8957f16bd76b96dcb60
George Siosi Samuels <george@faiacorp.com>
2025-03-09 21:45:16 -0600

    Update Convex API types for subscription management

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,10 +14,15 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as auth from "../auth.js";
+import type * as http_polar from "../http/polar.js";
 import type * as ideas from "../ideas.js";
+import type * as mutations_subscription from "../mutations/subscription.js";
+import type * as queries_subscription from "../queries/subscription.js";
+import type * as queries_userPreferences from "../queries/userPreferences.js";
 import type * as scorecards from "../scorecards.js";
 import type * as tasks from "../tasks.js";
 import type * as userPreferences from "../userPreferences.js";
+import type * as utils_subscription from "../utils/subscription.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -29,10 +34,15 @@ import type * as userPreferences from "../userPreferences.js";
  */
 declare const fullApi: ApiFromModules<{
   auth: typeof auth;
+  "http/polar": typeof http_polar;
   ideas: typeof ideas;
+  "mutations/subscription": typeof mutations_subscription;
+  "queries/subscription": typeof queries_subscription;
+  "queries/userPreferences": typeof queries_userPreferences;
   scorecards: typeof scorecards;
   tasks: typeof tasks;
   userPreferences: typeof userPreferences;
+  "utils/subscription": typeof utils_subscription;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
